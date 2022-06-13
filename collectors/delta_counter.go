1d1a83cadae19fe85e64f9e2c4696b7dbe166a29
Ozzie Sabina <(none)>
2022-06-13 19:19:56 -0400

    Add ability to treat some DELTA metrics as counter rather than gauge
    In order for us to be able to do some performance math on these in
    prometheus, we need to have counters.
    
    This code adds a CLI flag to define a substring (underscore delimited)
    that indicates a DELTA metric should be treated as a counter.
    
    This is accomplished by caching the "current" running value in this case
    and exporting that value as a counter.
    
    The helper code for this is in the collector namespace (I am using the
    fnv code and followed a similar pattern in adding it here).
    
    Currently this code only handles INT64 DELTAs.  I've identified a
    handful of DOUBLE DELTAs that could require similar treatment.  That's a
    TODO.

---
+++ collectors/delta_counter.go
@@ -0,0 +1,92 @@
+// Copyright 2020 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package collectors
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+	"time"
+)
+
+type CacheEntry struct {
+	val float64
+	ts  time.Time
+}
+
+var (
+	counterCache           = make(map[uint64]*CacheEntry)
+	counterSentinelStrings = []string{""}
+)
+
+func SetCounterSentinelStrings(strs []string) {
+	counterSentinelStrings = strs
+}
+
+func DeltaIsActuallyCounter(fq_name string) bool {
+	// For now we split on _ to get the _ separated words as we have found the word
+	// "count" in the metric name to be indicative of this delta-as-counter situation, but
+	// we may eventually want to simply look for a substring or RE for flexibility/specificity.
+	words := strings.Split(fq_name, "_")
+	for _, sentinel := range counterSentinelStrings {
+		for _, value := range words {
+			if value == sentinel {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func GetCounterValue(key uint64) float64 {
+	if entry, ok := counterCache[key]; ok {
+		return entry.val
+	}
+	newEntry := CacheEntry{ts: time.Time{}, val: 0}
+	counterCache[key] = &newEntry
+	return 0
+}
+
+func SetCounterValue(key uint64, val float64, ts time.Time) bool {
+	if entry, ok := counterCache[key]; ok {
+		if ts.After(entry.ts) {
+			newEntry := CacheEntry{ts: ts, val: val}
+			counterCache[key] = &newEntry
+			return true
+		}
+	}
+
+	return false
+}
+
+func SerializeLabels(keys []string, values []string) string {
+	labels := make(map[string]string)
+	labelstrs := []string{}
+	keysCopy := append([]string{}, keys...)
+	for i := range keys {
+		labels[keys[i]] = values[i]
+	}
+	sort.Strings(keysCopy)
+	for _, k := range keysCopy {
+		labelstrs = append(labelstrs, fmt.Sprintf("%s:%s", k, labels[k]))
+	}
+	return strings.Join(labelstrs, "|")
+}
+
+func GetCacheKey(fq_name string, keys []string, values []string) uint64 {
+	ptextkey := fq_name + "-" + SerializeLabels(keys, values)
+	h := hashNew()
+	h = hashAdd(h, ptextkey)
+	return h
+}
